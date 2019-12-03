4364b903e45e8d6d39b410df9aafdf4c8bef155a
Hideki Saito <hideki@hidekisaito.com>
2019-12-03 02:04:27 -0800

    Code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 mod callpass {
     #[allow(dead_code)]
-    pub fn get_callpass(mut call:String) -> i16 {
+    pub fn get_callpass(mut call: String) -> i16 {
         call = call.to_uppercase();
 
         let mut i = 0;
         let mut hash: i16 = 0x73e2;
 
         while i < call.len() {
-            hash = hash ^ (call.as_bytes()[i] as i16)<<8;
-            if i+1 < call.len() {
-                hash = hash ^ (call.as_bytes()[i+1] as i16);
+            hash = hash ^ (call.as_bytes()[i] as i16) << 8;
+            if i + 1 < call.len() {
+                hash = hash ^ (call.as_bytes()[i + 1] as i16);
             }
             i += 2;
         }
-        return hash & 0x7fff;
+        hash & 0x7fff
     }
 
     #[allow(dead_code)]
-    pub fn check_callpass(call:String, pass: i16) -> bool {
+    pub fn check_callpass(call: String, pass: i16) -> bool {
         let code = get_callpass(call);
-        return code == pass;
+        code == pass
     }
 }
 
@@ -35,7 +35,7 @@ mod tests {
 
     #[test]
     fn test_check_callpass() {
-        let result = callpass::check_callpass(String::from("TESTING"),31421);
+        let result = callpass::check_callpass(String::from("TESTING"), 31421);
         assert_eq!(result, true);
     }
 }
